b8016016cc19ce4494214055e685b24073ccf144
jrpolrot <jrpolrot@live.com>
2016-09-24 20:16:10 +0200

    code for generating plot 1 and resultat

---
+++ plot1.R
@@ -0,0 +1,18 @@
+
+# Loading the full dataset 
+fullset <- read.csv("household_power_consumption.txt",sep=";",header=TRUE) 
+# Filtering on the dates 
+filteredset <- subset(fullset,fullset$Date %in% c("1/2/2007","2/2/2007")) 
+# In place data conversions 
+filteredset$Time <- strptime(paste(filteredset$Date,filteredset$Time,sep = " "), "%d/%m/%Y %H:%M:%S") 
+filteredset$Date <- strptime(filteredset$Date,"%d/%m/%Y") 
+filteredset$Global_active_power <- as.numeric(as.character(filteredset$Global_active_power)) 
+filteredset$Voltage <- as.numeric(as.character(filteredset$Voltage)) 
+filteredset$Global_reactive_power <- as.numeric(as.character(filteredset$Global_reactive_power)) 
+filteredset$Sub_metering_1 <- as.numeric(as.character(filteredset$Sub_metering_1)) 
+filteredset$Sub_metering_2 <- as.numeric(as.character(filteredset$Sub_metering_2)) 
+filteredset$Sub_metering_3 <- as.numeric(as.character(filteredset$Sub_metering_3)) 
+ # Histogram drawing 
+png("Plot1.png",width= 480, height = 480,  units= "px") 
+hist(filteredset$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)") 
+dev.off() 
